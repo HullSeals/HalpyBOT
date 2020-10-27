59c4d0028a0cf46e50bfd69bcae4a2238d58d807
Rik Overveld <rikoverveld@gmail.com>
2020-10-27 21:46:59 +0000

    Merge privatecommandhandler

--- modules/facts/fact.py
+++ modules/facts/fact.py
@@ -8,4 +8,10 @@ async def go(bot: main, channel: str, sender: str, args: List[str]):
     if len(args) == 0:
         await bot.message(channel, facts["go_no_args"])
     else:
-        await bot.message(channel, f"{', '.join(str(seal) for seal in args)} {facts['go']})")
+        await bot.message(channel, f"{' '.join(str(seal) for seal in args)} {facts['go']})")
+
+async def help(bot: main, channel: str, sender: str, args: List[str]):
+    await bot.message(sender, facts["help"])
+
+async def about(bot: main, channel: str, sender: str, args: List[str]):
+    await bot.message(sender, facts["about"])
