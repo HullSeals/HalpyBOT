53f71d51ad5405fd999466bb657b92590db3e770
Rik079 <rikoverveld@gmail.com>
2020-10-27 12:08:38 +0100

    Clean up stuff

---
+++ config_example.py
@@ -0,0 +1,25 @@
+# This is an example file. To let it work, fill in all fields and rename this to config.py
+
+import logging
+
+class SASL:
+    identity = ''
+    username = ''
+    password = ''
+
+class IRC:
+    server = 'irc.hullseals.space'
+    port = 6697
+    useSsl = True
+    nickname = 'HalpyBOT_DEV'
+    commandPrefix = '!'
+    operline = ''
+    operlinePassword = ''
+
+class Logging:
+    filename = 'halpybot.log'
+    level = logging.DEBUG
+
+class ChannelArray:
+    channels = ["#bot-test",
+                "#seal-bob"]
