144920bd59f842f192176adf0d40f787a3d33cf8
Rik079 <rikoverveld@gmail.com>
2020-10-28 04:29:05 +0100

    [HALPY-13] Centralize fact-args handling

--- modules/facts/fact.py
+++ modules/facts/fact.py
@@ -3,20 +3,23 @@ from typing import List
 
 from .fact_list import facts
 
-
-async def go(bot: main, channel: str, sender: str, args: List[str], messagemode: int):
+async def recite_fact(bot: main, channel: str, sender: str, args: List[str], messagemode: int, fact: str):
     if messagemode == 1:
         if len(args) == 0:
-            await bot.message(channel, facts["go_no_args"])
+            await bot.message(channel, facts[f"{str(fact)}_no_args"])
         else:
-            await bot.message(channel, f"{' '.join(str(seal) for seal in args)} {facts['go']})")
+            await bot.message(channel, f"{' '.join(str(seal) for seal in args)} {facts[str(fact)]})")
     elif messagemode == 2:
         if len(args) == 0:
-            await bot.message(sender, facts["go_no_args"])
+            await bot.message(sender, facts[f"{str(fact)}_no_args"])
         else:
-            await bot.message(sender, f"{' '.join(str(seal) for seal in args)} {facts['go']})")
+            await bot.message(sender, f"{' '.join(str(seal) for seal in args)} {facts[str(fact)]})")
     else:
         return
+
+
+async def go(bot: main, channel: str, sender: str, args: List[str], messagemode: int):
+    await recite_fact(bot, channel, sender, args, messagemode, fact='go')
 
 
 async def help(bot: main, channel: str, sender: str, args: List[str], messagemode: int):
