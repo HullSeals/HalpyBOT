73c65b7e4ccca2f27043aedd84105f0adf3c45f6
Rik079 <rikoverveld@gmail.com>
2020-10-29 03:08:53 +0100

    [Fix] Remove extra space from fact

--- modules/facts/fact.py
+++ modules/facts/fact.py
@@ -32,7 +32,7 @@ async def recite_fact(bot: main, channel: str, sender: str, args: List[str], mes
         if len(args) == 0:
             return await bot.message(sender, facts[str(f"{fact}_no_args")])
         else:
-            return await bot.message(sender, f"{' '.join(str(seal) for seal in args)} {facts[str(fact)]})")
+            return await bot.message(sender, f"{' '.join(str(seal) for seal in args)}{facts[str(fact)]})")
     # PM only, 1 version
     if f"{fact}_no_args" not in facts and messagemode == 2 and req_dm is True:
         return await bot.message(sender, facts[str(fact)])
@@ -47,11 +47,11 @@ async def recite_fact(bot: main, channel: str, sender: str, args: List[str], mes
         if len(args) == 0:
             await bot.message(channel, facts[f"{str(fact)}_no_args"])
         else:
-            await bot.message(channel, f"{' '.join(str(seal) for seal in args)} {facts[str(fact)]}")
+            await bot.message(channel, f"{' '.join(str(seal) for seal in args)}{facts[str(fact)]}")
     elif messagemode == 2:
         if len(args) == 0:
             await bot.message(sender, facts[f"{str(fact)}_no_args"])
         else:
-            await bot.message(sender, f"{' '.join(str(seal) for seal in args)} {facts[str(fact)]}")
+            await bot.message(sender, f"{' '.join(str(seal) for seal in args)}{facts[str(fact)]}")
     else:
         return
