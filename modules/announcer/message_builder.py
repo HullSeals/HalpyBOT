5b6d30181b3f1f1e4fdf2a63f9b4c964a164a712
Rik079 <rikoverveld@gmail.com>
2020-10-30 17:21:44 +0100

    [HALPY-18] Add code black announcer

--- modules/announcer/message_builder.py
+++ modules/announcer/message_builder.py
@@ -3,4 +3,10 @@ import main
 from typing import List
 
 async def codeblack(bot: main, channel: str, sender: str, args: List[str]):
-    print("Test")
+    send_to = ["#Repair-Requests", "#Code-Black", "#seal-bob"]
+    message = f"xxxx CBCASE xxxx \n" \
+              f"CMDR: {args[0]} -- Platform: {args[1]} System: {args[2]} -- Hull: {args[3]} \n" \
+              f"Can synth: {args[4]} -- O2 timer: {args[5]} \n" \
+              f"xxxx NEWCASE xxxx"
+    for ch in send_to:
+        await bot.message(ch, message)
