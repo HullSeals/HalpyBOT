0e566fa415b725fd06d6fce90297586e626da8af
Rik079 <rikoverveld@gmail.com>
2020-11-02 21:09:20 +0100

    [HALPY-29] Add CaseNotify message for manual cases

--- modules/announcer/manual_case.py
+++ modules/announcer/manual_case.py
@@ -2,29 +2,33 @@ from typing import List
 import main
 from modules.util.checks import require_permission, DeniedMessage
 import logging
+from ..util.checks import require_channel
 
 send_to = ["#Repair-Requests", "#Code-Black", "#seal-bob"]
 
+@require_channel()
 @require_permission("DRILLED", message=DeniedMessage.DRILLED)
 async def manual_case(bot: main, channel: str, sender: str, args: List[str], in_channel: bool):
-    if in_channel:
-        message = f"xxxx MANCASE xxxx\n" \
-                  f"{' '.join(args)}\n" \
-                  f"xxxx NEWCASE xxxx"
-        for ch in send_to:
-            await bot.message(ch, message)
-            logging.info(f"Manual case by {sender} in {channel}: {args}")
-    else:
-        return
+    message = f"xxxx MANCASE xxxx\n" \
+              f"{' '.join(args)}\n" \
+              f"xxxx NEWCASE xxxx"
+    for ch in send_to:
+        await bot.message(ch, message)
+        logging.info(f"Manual case by {sender} in {channel}: {args}")
+    cn_message = f"New Manual Case Available -- <@&744998165714829334>\n" \
+                 f"{' '.join(args)}"
+    await bot.message("#case-notify", cn_message)
 
+
+@require_channel()
 @require_permission("DRILLED", message=DeniedMessage.DRILLED)
 async def manual_kingfisher(bot: main, channel: str, sender: str, args: List[str], in_channel: bool):
-    if in_channel:
-        message = f"xxxx MANKFCASE xxxx\n" \
-                  f"{' '.join(args)}\n" \
-                  f"xxxx NEWKFCASE xxxx"
-        for ch in send_to:
-            await bot.message(ch, message)
-            logging.info(f"Manual kingfisher case by {sender} in {channel}: {args}")
-    else:
-        return
+    message = f"xxxx MANKFCASE xxxx\n" \
+              f"{' '.join(args)}\n" \
+              f"xxxx NEWKFCASE xxxx"
+    for ch in send_to:
+        await bot.message(ch, message)
+        logging.info(f"Manual kingfisher case by {sender} in {channel}: {args}")
+    cn_message = f"New Manual KFCase Available -- <@&744998165714829334>\n" \
+                 f"{' '.join(args)}"
+    await bot.message("#case-notify", cn_message)
