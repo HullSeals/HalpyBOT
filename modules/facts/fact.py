9d96a77d257b1ef4778cc63e59499912c785208f
Rik079 <rikoverveld@gmail.com>
2020-11-06 18:13:56 +0100

    [HALPY-23] Remove (almost) all no_args facts

--- modules/facts/fact.py
+++ modules/facts/fact.py
@@ -5,6 +5,7 @@ from .fact_list import facts
 
 
 async def recite_fact(bot: main, channel: str, sender: str, args: List[str], in_channel: bool, fact: str):
+
     # Check if fact is DM only
     req_dm = False
     if facts[str(fact)][0] is True:
@@ -12,43 +13,44 @@ async def recite_fact(bot: main, channel: str, sender: str, args: List[str], in_
     elif facts[str(fact)][0] is False:
         req_dm = False
     else:
-        msg = "Fact not properly registered! Contact a Cyberseal"
-        if in_channel:
-            return await bot.message(channel, msg)
-        elif in_channel is False:
-            return await bot.message(sender, msg)
+        bot.reply(channel, sender, in_channel, "Fact not properly registered! Contact a Cyberseal")
+
     # Check if fact is present
     if str(fact) not in facts:
-        msg = "Couldn't find fact! contact a Cyberseal"
-        if in_channel:
-            return await bot.message(channel, msg)
-        elif in_channel is False:
-            return await bot.message(sender, msg)
+        bot.reply(channel, sender, in_channel, "Couldn't find fact! contact a Cyberseal")
+
     # PM only, noargs and args
     if f"{fact}_no_args" in facts and in_channel is False and req_dm is True:
         if len(args) == 0:
             return await bot.message(sender, facts[str(f"{fact}_no_args")][1])
         else:
-            return await bot.message(sender, f"{' '.join(str(seal) for seal in args)}{facts[str(fact)][1]})")
+            return await bot.message(sender, f"{' '.join(str(seal) for seal in args)}: {facts[str(fact)][1]})")
+
     # PM only, 1 version
     if f"{fact}_no_args" not in facts and in_channel is False and req_dm is True:
-        return await bot.message(sender, facts[str(fact)])
+        if len(args) == 0:
+            return await bot.message(sender, facts[str(fact)])
+        else:
+            return await bot.message(sender, f"{' '.join(str(seal) for seal in args)}: {facts[str(fact)][1]})")
+
     # Public and PM, 1 version
     if f"{fact}_no_args" not in facts and req_dm is False:
-        if in_channel:
-            return await bot.message(channel, facts[str(fact)][1])
+        if len(args) == 0:
+            return await bot.reply(channel, sender, in_channel, facts[str(fact)][1])
         else:
-            return await bot.message(sender, facts[str(fact)][1])
+            return await bot.reply(channel, sender, in_channel,
+                                   f"{' '.join(str(seal) for seal in args)}: {facts[str(fact)][1]})")
+
     # Public and PM, args and noargs
     if in_channel and req_dm is False:
         if len(args) == 0:
             await bot.message(channel, facts[f"{str(fact)}_no_args"][1])
         else:
-            await bot.message(channel, f"{' '.join(str(seal) for seal in args)}{facts[str(fact)][1]}")
+            await bot.message(channel, f"{' '.join(str(seal) for seal in args)}: {facts[str(fact)][1]}")
     elif in_channel is False:
         if len(args) == 0:
             await bot.message(sender, facts[f"{str(fact)}_no_args"])
         else:
-            await bot.message(sender, f"{' '.join(str(seal) for seal in args)}{facts[str(fact)][1]}")
+            await bot.message(sender, f"{' '.join(str(seal) for seal in args)}: {facts[str(fact)][1]}")
     else:
         return
--- modules/facts/fact_list.py
+++ modules/facts/fact_list.py
@@ -8,151 +8,88 @@ facts = {
               "in the development of HalpyBOT."],
 
     "go": [False,
-           ", You're up. All others, stand down."],
+           "You're up. All others, stand down."],
 
     "go_no_args": [False,
                    "Mentioned Seals, You're up. All others, stand down."],
 
     "beacon": [False,
-               ": To turn on your beacon, Go to the right-hand menu, "
+               "To turn on your beacon, Go to the right-hand menu, "
                "and select to the 'Ship' tab. (1/2) \n"
                "Under Functions, select 'Beacon', and then select 'Wing'. (2/2)"],
 
-    "beacon_no_args": [False,
-                       "Client: To turn on your beacon, Go to the right-hand menu, "
-                       "and select to the 'Ship' tab. (1/2) \n"
-                       "Under Functions, select 'Beacon', and then select 'Wing'. (2/2)"],
-
     "cbinfo": [False,
-               ": DO NOT LOG IN and provide the following info if you know it: (1/2) \n"
+               "DO NOT LOG IN and provide the following info if you know it: (1/2) \n"
                "CMDR Name, Current System, Hull %, O2 Time Left, "
                "Platform, and if you have any life support synthesis. (2/2)"],
 
-    "cbinfo_no_args": [False,
-                       "DO NOT LOG IN and provide the following info if you know it: (1/2) \n"
-                       "CMDR Name, Current System, Hull %, O2 Time Left, "
-                       "Platform, and if you have any life support synthesis. (2/2)"],
-
     "cbmining": [False,
-                 ": You can find the location of objects using the radar module. "
+                 "You can find the location of objects using the radar module. "
                  "Select the item you want on the left hand panel, then go towards it using your radar. (1/2) \n"
                  "Open your cargo scoop, and then go towards the item. Move your ship above the item slightly, "
                  "then move forward with a speed of less then 40. (2/2)"],
 
-    "cbmining_no_args": [False,
-                         "Client: You can find the location of objects using the radar module. "
-                         "Select the item you want on the left hand panel, "
-                         "then go towards it using your radar. (1/2) \n"
-                         "Open your cargo scoop, and then go towards the item. Move your ship above the item slightly, "
-                         "then move forward with a speed of less then 40. (2/2)"],
-
     "clientinfo": [False,
-                   ": Greetings, CMDR. Please provide the following information: (1/2) \n"
+                   "Greetings, CMDR. Please provide the following information: (1/2) \n"
                    "CMDR Name, Current System, Hull %, and Platform. If your canopy is breached, "
                    "LOG OUT IMMEDIATELY. (2/2)"],
 
-    "clientinfo_no_args": [False,
-                           "Greetings, CMDR. Please provide the following information: (1/2) \n"
-                           "CMDR Name, Current System, Hull %, and Platform. If your canopy is breached, "
-                           "LOG OUT IMMEDIATELY. (2/2)"],
-
     "escapeneutron": [False,
-                      ": Guide to Escaping a Neutron Cone (1/2) \n"
+                      "Guide to Escaping a Neutron Cone (1/2) \n"
                       "Text: https://hullse.al/enText --- Video: https://hullse.al/enVid (2/2)"],
 
-    "escapeneutron_no_args": [False,
-                              "Here's a Guide to Escaping a Neutron Cone (1/2) \n"
-                              "Text: https://hullse.al/enText --- Video: https://hullse.al/enVid (2/2)"],
-
     "paperwork": [False,
-                  ": Don't forget to do your paperwork! http://hullse.al/ppwk"],
-
-    "paperwork_no_args": [False,
-                          "Seal: Don't forget to do your paperwork! http://hullse.al/ppwk"],
+                  "Don't forget to do your paperwork! http://hullse.al/ppwk"],
 
     "pcfr": [False,
-             ": Please tap ESC to go to the PAUSE menu, click SOCIAL, "
+             "Please tap ESC to go to the PAUSE menu, click SOCIAL, "
              "enter the CMDR name in the box, and click ADD FRIEND on their name."],
 
-    "pcfr_no_args": [False,
-                     "Client: Please tap ESC to go to the PAUSE menu, click SOCIAL, "
-                     "enter the CMDR name in the box, and click ADD FRIEND on their name."],
-
     "psfr": [False,
-             ": Please tap your PlayStation button, then open the Friends screen from Functions, "
+             "Please tap your PlayStation button, then open the Friends screen from Functions, "
              "search for your Seal's name, and add them as your friend."],
 
-    "psfr_no_args": [False,
-                     "Client: Please tap your PlayStation button, then open the Friends screen from Functions, "
-                     "search for your Seal's name, and add them as your friend."],
-
     "xbfr": [False,
-             ": Please tap your XBOX button, then go to the Social tab "
+             "Please tap your XBOX button, then go to the Social tab "
              "and search for your Seal's name. Be sure to add the Seal(s) as your friend."],
 
-    "xbfr_no_args": [False,
-                     "Client: Please tap your XBOX button, then go to the Social tab "
-                     "and search for your Seal's name. Be sure to add the Seal(s) as your friend."],
-
     "wing": [False,
-             ": Please open your Comms Panel (top left option), and select the third tab. "
+             "Please open your Comms Panel (top left option), and select the third tab. "
              "Then, select the Seal(s) you want, and select Invite to Wing."],
 
-    "wing_no_args": [False,
-                     "Client: Please open your Comms Panel (top left option), and select the third tab. "
-                     "Then, select the Seal(s) you want, and select Invite to Wing."],
-
     "stuck": [False,
-              ": Greetings, CMDR. Please provide the following information: (1/2) \n"
+              "Greetings, CMDR. Please provide the following information: (1/2) \n"
               "CMDR Name, Current System, Current Planet/Moon, Your Current Coordinates, "
               "your Platform, and what services you require! (2/2)"],
 
-    "stuck_no_args": [False,
-                      "Greetings, CMDR. Please provide the following information: (1/2) \n"
-                      "CMDR Name, Current System, Current Planet/Moon, Your Current Coordinates, "
-                      "your Platform, and what services you require! (2/2)"],
-
     "prep": [False,
-             ": If you are landed, take off and move to at least 5Ls from any body. (1/2) \n"
+             "If you are landed, take off and move to at least 5Ls from any body. (1/2) \n"
              " Please drop from Supercruise, set your throttle to zero, "
              "and deploy your landing gear while you wait. (2/2)"],
 
-    "prep_no_args": [False,
-                     "Client: If you are landed, take off and move to at least 5Ls from any body. (1/2) \n"
-                     "Please drop from Supercruise, set your throttle to zero, "
-                     "and deploy your landing gear while you wait. (2/2)"],
-
     "verify": [False,
-               ": We just need to check some things. Please confirm for us: (1/4) \n"
+               "We just need to check some things. Please confirm for us: (1/4) \n"
                "Your Current Game Mode, CMDR name, and current system. (2/4) \n"
                "**Please also provide a screenshot of your cockpit view, with system and date visible. "
                "We encourage programs like imgur or Discord for image hosting. (3/4)** \n"
                "Seals, please hold position while we check. (4/4)"],
 
-    "verify_no_args": [False,
-                       "We just need to check some things, CMDR. Please confirm for us: (1/4) \n"
-                       " Your Current Game Mode, CMDR name, and current system. (2/4) \n"
-                       "**Please also provide a screenshot of your cockpit view, with system and date visible. "
-                       "We encourage programs like imgur or Discord for image hosting. (3/4)** \n"
-                       "Seals, please hold position while we check. (4/4)"],
-
     "chatter": [False,
-                "- - - - - - - - - - - - - - - -  \n"
                 "Seals, please keep chatter to #seal-bob. Thank you."],
 
-    "join": [False,
-             ": Greetings, Welcome to the Hull Seals! "
-             "Here's some info to get you started on joining us: https://hullse.al/How2Join"],
+    "chatter_no_args": [False,
+                        "- - - - - - - - - - - - - - - -  \n"
+                        "Seals, please keep chatter to #seal-bob. Thank you."],
 
-    "join_no_args": [False,
-                     "Greetings, CMDR! Welcome to the Hull Seals. "
-                     "Here's some info to get you started on joining us: https://hullse.al/How2Join"],
+    "join": [False,
+             "Greetings, Welcome to the Hull Seals! "
+             "Here's some info to get you started on joining us: https://hullse.al/How2Join"],
     
     "bacon": [False,
               "The bacon is lit! -- https://youtu.be/Wd2qRSzCj84"],
 
     "fuel": [False,
-             ": For fuel emergencies, your best bet is the Fuel Rats: "
+             "For fuel emergencies, your best bet is the Fuel Rats: "
              "https://fuelrats.com/i-need-fuel"],
 
     "fuel_no_args": [False,
@@ -160,57 +97,32 @@ facts = {
                      "https://fuelrats.com/i-need-fuel"],
 
     "cmdlist": [False,
-                ": Common HalpyBOT Commands: http://hullse.al/cmdlist"],
-
-    "cmdlist_no_args": [False,
-                        "Common HalpyBOT Commands: http://hullse.al/cmdlist"],
+                "Common HalpyBOT Commands: http://hullse.al/cmdlist"],
 
     "welcome": [False,
-                ": Welcome to the Hull Seals! "
+                "Welcome to the Hull Seals! "
                 "We've got your case details and we'll be with you shortly. "
                 "Just take a breath, stay calm, and follow all instructions given to you by Dispatchers."],
 
-    "welcome_no_args": [False,
-                        "Welcome to the Hull Seals! "
-                        "We've got your case details and we'll be with you shortly. "
-                        "Just take a breath, stay calm, and follow all instructions given to you by Dispatchers."],
-
     "tos": [False,
-            ": Our Terms of Service and Important Info: https://hullse.al/ImportantInfo"],
-
-    "tos_no_args": [False,
-                    "Our Terms of Service and Important Info: https://hullse.al/ImportantInfo"],
+            "Our Terms of Service and Important Info: https://hullse.al/ImportantInfo"],
 
     "highg": [False,
-              ": Remain landed until instructed by your dispatcher. "
+              "Remain landed until instructed by your dispatcher. "
               "When instructed, use your VERTICAL THRUSTERS ONLY "
               "to rise up to at least 6km above the surface. (1/2) \n"
               "Keep your landing gear deployed until you are ready to jump to supercruise, "
               "and keep your ship aligned horizontally with the planet at all times.  (2/2)"],
 
-    "highg_no_args": [False,
-                      "Client: Remain landed until instructed by your dispatcher. "
-                      "When instructed, use your VERTICAL THRUSTERS ONLY "
-                      "to rise up to at least 6km above the surface. (1/2) \n"
-                      "Keep your landing gear deployed until you are ready to jump to supercruise, "
-                      "and keep your ship aligned horizontally with the planet at all times.  (2/2)"],
-
     "synth": [False,
-              ": To synth a new life support refill, "
+              "To synth a new life support refill, "
               "Go to the right-hand menu, and select to the 'Inventory' tab. (1/3) \n"
               "Go down to 'Synthesis' (the second to last option), choose 'Life Support', "
               "and then select 'Resupply Life Support'. (2/3) \n"
               "This requires you have 2 iron and 1 nickel in your materials reserve. (3/3)"],
 
-    "synth_no_args": [False,
-                      "Client: To synth a new life support refill, "
-                      "Go to the right-hand menu, and select to the 'Inventory' tab. (1/3) \n"
-                      "Go down to 'Synthesis' (the second to last option), choose 'Life Support', "
-                      "and then select 'Resupply Life Support'. (2/3) \n"
-                      "This requires you have 2 iron and 1 nickel in your materials reserve. (3/3)"],
-
     "fact_test": [False,
-                  ": This is a test fact for the cybers. It does nothing useful."],
+                  "This is a test fact for the cybers. It does nothing useful."],
 
     "fact_test_no_args": [False,
                           "This is a test fact for the cybers. It does nothing useful."],
