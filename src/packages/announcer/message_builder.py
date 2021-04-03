df18bb34b267485c74d59488191f0f67bdbe39a9
David Sangrey <davidsangrey@gmail.com>
2021-04-03 21:04:31 +0000

    Update message_builder.py

--- src/packages/announcer/message_builder.py
+++ src/packages/announcer/message_builder.py
@@ -1,5 +1,5 @@
 """
-HalpyBOT v1.4
+HalpyBOT v1.3.1
 
 message_builder.py - Build the messages for the announcer
 
@@ -8,37 +8,54 @@ All rights reserved.
 
 Licensed under the GNU General Public License
 See license.md
-
-This module is due for a rewrite, and not documented
-
 """
 
 from ..edsm import *
 from typing import List
-from ...packages.configmanager import config
+import main
 
+def send_to(annmodule: str, type: str):
+    return [entry.strip() for entry in main.config.get(annmodule, type).split(',')]
 
 async def codeblack(ctx, args: List[str]):
-    message = f"xxxx CBCASE -- NEWCASE -- {args[1]}CASE xxxx \n" \
+    message = f"xxxx CBCASE -- NEWCASE xxxx \n" \
               f"CMDR: {args[0]} -- Platform: {args[1]} System: {args[2]} -- Hull: {args[3]} \n" \
               f"Can synth: {args[4]} -- O2 timer: {args[5]} \n" \
               f"xxxxxxxx"
     message2 = await casecheck(ctx, sys=args[2])
-    for ch in config['Announcer.cases']['channels'].split():
+    for ch in send_to('Announcer.cases', 'channels'):
         await ctx.bot.message(ch, message)
         await ctx.bot.message(ch, message2)
 
-
-async def case(ctx, args: List[str]):
-    message = f"xxxx {args[1]}CASE -- NEWCASE xxxx \n" \
+async def pc(ctx, args: List[str]):
+    message = f"xxxx PCCASE -- NEWCASE xxxx \n" \
               f"CMDR: {args[0]} -- Platform: {args[1]} \n" \
               f"System: {args[2]} -- Hull: {args[3]} \n" \
               f"xxxxxxxx"
     message2 = await casecheck(ctx, sys=args[2])
-    for ch in config['Announcer.cases']['channels'].split():
+    for ch in send_to('Announcer.cases', 'channels'):
         await ctx.bot.message(ch, message)
         await ctx.bot.message(ch, message2)
 
+async def xb(ctx, args: List[str]):
+    message = f"xxxx XBCASE -- NEWCASE xxxx \n" \
+              f"CMDR: {args[0]} -- Platform: {args[1]} \n" \
+              f"System: {args[2]} -- Hull: {args[3]} \n" \
+              f"xxxxxxxx"
+    message2 = await casecheck(ctx, sys=args[2])
+    for ch in send_to('Announcer.cases', 'channels'):
+        await ctx.bot.message(ch, message)
+        await ctx.bot.message(ch, message2)
+
+async def ps(ctx, args: List[str]):
+    message = f"xxxx PSCASE -- NEWCASE xxxx \n" \
+              f"CMDR: {args[0]} -- Platform: {args[1]} \n" \
+              f"System: {args[2]} -- Hull: {args[3]} \n" \
+              f"xxxxxxxx"
+    message2 = await casecheck(ctx, sys=args[2])
+    for ch in send_to('Announcer.cases', 'channels'):
+        await ctx.bot.message(ch, message)
+        await ctx.bot.message(ch, message2)
 
 async def plterr(ctx, args: List[str]):
     message = f"xxxx PLATFORM_ERROR -- NEWCASE xxxx \n" \
@@ -46,23 +63,45 @@ async def plterr(ctx, args: List[str]):
               f"System: {args[2]} -- Hull: {args[3]} \n" \
               f"xxxxxxxx"
     message2 = await casecheck(ctx, sys=args[2])
-    for ch in config['Announcer.cases']['channels'].split():
+    for ch in send_to('Announcer.cases', 'channels'):
         await ctx.bot.message(ch, message)
         await ctx.bot.message(ch, message2)
 
-
-async def kingfisher(ctx, args: List[str]):
-    message = f"xxxx {args[1]}KFCASE -- NEWCASE xxxx\n" \
+async def kingfisher_xb(ctx, args: List[str]):
+    message = f"xxxx XBKFCASE -- NEWCASE xxxx\n" \
               f"CMDR: {args[0]} -- Platform: {args[1]}\n" \
               f"System: {args[2]} -- Planet: {args[3]}\n" \
               f"Coordinates: {args[4]}\n" \
               f"Type: {args[5]}\n" \
               f"xxxxxxxx"
     message2 = await casecheck(ctx, sys=args[2])
-    for ch in config['Announcer.cases']['channels'].split():
+    for ch in send_to('Announcer.cases', 'channels'):
         await ctx.bot.message(ch, message)
         await ctx.bot.message(ch, message2)
 
+async def kingfisher_pc(ctx, args: List[str]):
+    message = f"xxxx PCKFCASE -- NEWCASE xxxx\n" \
+              f"CMDR: {args[0]} -- Platform: {args[1]}\n" \
+              f"System: {args[2]} -- Planet: {args[3]}\n" \
+              f"Coordinates: {args[4]}\n" \
+              f"Type: {args[5]}\n" \
+              f"xxxxxxxx"
+    message2 = await casecheck(ctx, sys=args[2])
+    for ch in send_to('Announcer.cases', 'channels'):
+        await ctx.bot.message(ch, message)
+        await ctx.bot.message(ch, message2)
+
+async def kingfisher_ps(ctx, args: List[str]):
+    message = f"xxxx PSKFCASE -- NEWCASE xxxx\n" \
+              f"CMDR: {args[0]} -- Platform: {args[1]}\n" \
+              f"System: {args[2]} -- Planet: {args[3]}\n" \
+              f"Coordinates: {args[4]}\n" \
+              f"Type: {args[5]}\n" \
+              f"xxxxxxxx"
+    message2 = await casecheck(ctx, sys=args[2])
+    for ch in send_to('Announcer.cases', 'channels'):
+        await ctx.bot.message(ch, message)
+        await ctx.bot.message(ch, message2)
 
 async def kingfisher_plterr(ctx, args: List[str]):
     message = f"xxxx PLATFORM_ERROR -- NEWCASE xxxx\n" \
@@ -72,13 +111,13 @@ async def kingfisher_plterr(ctx, args: List[str]):
               f"Type: {args[5]}\n" \
               f"xxxxxxxx"
     message2 = await casecheck(ctx, sys=args[2])
-    for ch in config['Announcer.cases']['channels'].split():
+    for ch in send_to('Announcer.cases', 'channels'):
         await ctx.bot.message(ch, message)
         await ctx.bot.message(ch, message2)
 
 
 async def ppwk(ctx, args: List[str]):
-    for ch in config['Announcer.paperwork']['channels'].split():
+    for ch in send_to('Announcer.paperwork', 'channels'):
         await ctx.bot.message(ch, f"Paperwork for case {args[0]} completed by {args[1]}")
 
 
