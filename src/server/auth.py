65a4904870087017d5021420ab86ef22a6d9c63c
David Sangrey <rixxan@hullseals.space>
2021-10-12 16:57:52 -0400

    Remove CRLF/LF catches, avoid the problem instead of writing code to fix it.

--- src/server/auth.py
+++ src/server/auth.py
@@ -28,22 +28,15 @@ def Authenticate():
             data = await request.json()
             clientmac = request.headers.get('hmac')
             msg = json.dumps(data, indent=4)
+            msg = ''.join(msg.split())
 
-            # Need CRLF (\r\n) instead of just LF
-            # Not sure who's to blame for switching them. Leading candidate is maybe Ubuntu but Python is also a suspect
-            CRLF_msg = msg.replace("\n", "\r\n")
-            LF_msg = msg
-
-            LF_mac = hmac.new(bytes(client_secret, 'utf8'), msg=LF_msg.encode('utf8'), digestmod=hashlib.sha256)
-            CRLF_mac = hmac.new(bytes(client_secret, 'utf8'), msg=CRLF_msg.encode('utf8'), digestmod=hashlib.sha256)
-
+            mac = hmac.new(bytes(client_secret, 'utf8'), msg=msg.encode('utf8'), digestmod=hashlib.sha256)
             keyCheck = request.headers.get('keyCheck')
             check = hmac.new(bytes(client_secret, 'utf8'), msg = checkConstant.encode('utf8'), digestmod=hashlib.sha256)
 
             if not hmac.compare_digest(keyCheck, check.hexdigest()):
                 raise web.HTTPUnauthorized()
-            # Allows request body to use either CRLF or LF for new line. Should prevent this problem in the future
-            elif not (hmac.compare_digest(clientmac, LF_mac.hexdigest()) or hmac.compare_digest(clientmac, CRLF_mac.hexdigest())):
+            elif not hmac.compare_digest(clientmac, mac.hexdigest()):
                 raise web.HTTPBadRequest()
             else:
                 return await function(request)
