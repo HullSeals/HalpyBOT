5fb224340c6457404fc82f13d55272a0ca3b3e0f
David Sangrey <rixxan@hullseals.space>
2022-02-12 14:41:05 -0500

    [FIX] Set default User Agent with aioHTTP
    Fixes an issue where the default user agent had been set with Requests, which we no longer use in favor of AIOHTTP. This fix reapplies the user agent to each of the existing aiohttp clientsessions, while removing Requests code from __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,5 @@
-from requests import utils
 from src.packages.configmanager import config
 
 __version__ = "1.5"
 
 DEFAULT_USER_AGENT = config['UserAgent']['agent_value'] + "/" + __version__ + " " + config['UserAgent']['agent_comment']
-utils.default_user_agent = lambda: DEFAULT_USER_AGENT
