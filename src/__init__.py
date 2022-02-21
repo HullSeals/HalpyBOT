ac49041b3734a51a401fd327a8d4948817f57669
David Sangrey <rixxan@hullseals.space>
2022-02-21 18:36:41 -0500

    Improve the UserAgent response & Config File
    Updates documentation in the Config file for Twitter, Removes default key from API connector, updates UserAgent to a standard format, and updates potential response from GET / route.

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,6 @@ from src.packages.configmanager import config
 
 __version__ = "1.5.2"
 
-DEFAULT_USER_AGENT = config['UserAgent']['agent_value'] + "/" + __version__ + " " + config['UserAgent']['agent_comment']
+# Normal User Agent string follows: Product/version (<system-information>) <platform> (<platform-details>)
+DEFAULT_USER_AGENT = "HalpyBOT/" + __version__ + " (" + config['IRC']['nickname'] + ") "\
+                     + config['UserAgent']['agent_comment']
