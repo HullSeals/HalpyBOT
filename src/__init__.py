d266f9a2fafaf6f763d675c798b78b07719f8870
David Sangrey <rixxan@hullseals.space>
2022-02-21 18:42:07 -0500

    [Minor] Remove Debug Comment

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,5 @@ from src.packages.configmanager import config
 
 __version__ = "1.5.2"
 
-# Normal User Agent string follows: Product/version (<system-information>) <platform> (<platform-details>)
 DEFAULT_USER_AGENT = "HalpyBOT/" + __version__ + " (" + config['IRC']['nickname'] + ") "\
                      + config['UserAgent']['agent_comment']
