acf96435a8f306aaa48a646e7d2038218402ecb8
David Sangrey <rixxan@hullseals.space>
2022-04-07 02:47:26 -0400

    [Halpy-120] Add !lookup tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -12,6 +12,8 @@ See license.md
 import pytest
 from halpybot.packages.command import Commands
 from halpybot.packages.configmanager import config
+# noinspection PyUnresolvedReferences
+from .mock_edsm import mock_api_server_fx
 
 
 @pytest.mark.asyncio
@@ -20,3 +22,35 @@ async def test_serverping(bot_fx):
         bot=bot_fx, channel="#bot-test", sender="generic_seal", message=f"{config['IRC']['commandprefix']}ping"
     )
     assert bot_fx.sent_messages[0] == {"message": "Pong!", "target": "#bot-test"}
+
+
+@pytest.mark.asyncio
+async def test_lookup(bot_fx, mock_api_server_fx):
+    await Commands.invoke_from_message(
+        bot=bot_fx, channel="#bot-test", sender="generic_seal", message=f"{config['IRC']['commandprefix']}lookup Sol"
+    )
+    assert bot_fx.sent_messages[0] == {'message': 'System SOL exists in EDSM', "target": "#bot-test"}
+
+
+@pytest.mark.asyncio
+async def test_lookup_2(bot_fx, mock_api_server_fx):
+    await Commands.invoke_from_message(
+        bot=bot_fx, channel="#bot-test", sender="generic_seal", message=f"{config['IRC']['commandprefix']}lookup"
+    )
+    assert bot_fx.sent_messages[0] == {'message': 'Use: ^lookup [System Name]\nAliases: syslookup\nCheck if a system exists in EDSM', "target": "#bot-test"}
+
+
+@pytest.mark.asyncio
+async def test_lookup_3(bot_fx, mock_api_server_fx):
+    await Commands.invoke_from_message(
+        bot=bot_fx, channel="#bot-test", sender="generic_seal", message=f"{config['IRC']['commandprefix']}lookup blargh"
+    )
+    assert bot_fx.sent_messages[0] == {'message': 'System BLARGH not found in EDSM', "target": "#bot-test"}
+
+
+@pytest.mark.asyncio
+async def test_lookup_4(bot_fx, mock_api_server_fx):
+    await Commands.invoke_from_message(
+        bot=bot_fx, channel="#bot-test", sender="generic_seal", message=f"{config['IRC']['commandprefix']}lookup --new Sol"
+    )
+    assert bot_fx.sent_messages[0] == {'message': 'System SOL exists in EDSM', "target": "#bot-test"}
