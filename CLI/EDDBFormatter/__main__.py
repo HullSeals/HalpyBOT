03b804cf56e799c50a470079e3c5b4f4c56286ba
David Sangrey <rixxan@hullseals.space>
2022-05-23 10:36:53 -0400

    [Halpy-136] Add Final File Generator

--- CLI/EDDBFormatter/__main__.py
+++ CLI/EDDBFormatter/__main__.py
@@ -25,7 +25,7 @@ def run():
     )
     print(
         "WARNING: This operation will remove the existing generated files, and relies on filtered data dumps from "
-        "EDDB to proceed. \n "
+        "EDDB to proceed."
     )
     cont = input(
         "Please make a backup of the previously generated files first. Do you wish to proceed? (Y/n) "
@@ -35,17 +35,20 @@ def run():
         sys.exit()
 
     # Remove Old Files
-    if os.path.exists("EDDBFormatter/files/filtered_stations.json"):
-        os.remove("EDDBFormatter/files/filtered_stations.json")
+    if os.path.exists("EDDBFormatter/files/output/filtered_stations.json"):
+        os.remove("EDDBFormatter/files/output/filtered_stations.json")
 
-    if os.path.exists("EDDBFormatter/files/filtered_systems_populated.json"):
-        os.remove("EDDBFormatter/files/filtered_systems_populated.json")
+    if os.path.exists("EDDBFormatter/files/output/filtered_systems_populated.json"):
+        os.remove("EDDBFormatter/files/output/filtered_systems_populated.json")
 
-    with open("EDDBFormatter/files/formatted_systems_populated.json", "r") as systemfile:
+    if os.path.exists("EDDBFormatter/files/output/filtered_combined_stations_with_systems.json"):
+        os.remove("EDDBFormatter/files/output/filtered_combined_stations_with_systems.json")
+
+    with open("EDDBFormatter/files/input/formatted_systems_populated.json", "r") as systemfile:
         system_data = json.load(systemfile)
-        third_dict = {}
+        system_dict = {}
         counter = 1
-        for key_2 in tqdm(system_data):
+        for key_2 in tqdm(system_data, desc="Filtering System Data: "):
             temp_dict_2 = {
                 "id": key_2["id"],
                 "system_name": key_2["name"],
@@ -55,17 +58,17 @@ def run():
                 "needs_permit": key_2["needs_permit"],
             }
             if temp_dict_2["needs_permit"] is not True:
-                third_dict[counter] = temp_dict_2
+                system_dict[key_2["id"]] = temp_dict_2
                 counter += 1
 
-        with open("EDDBFormatter/files/filtered_systems_populated.json", "w") as system_file:
-            json.dump(third_dict, system_file, indent=2)
+        with open("EDDBFormatter/files/output/filtered_systems_populated.json", "w") as system_file:
+            json.dump(system_dict, system_file, indent=2)
 
-    with open("EDDBFormatter/files/formatted_stations.json", "r") as jsonfile:
+    with open("EDDBFormatter/files/input/formatted_stations.json", "r") as jsonfile:
         data = json.load(jsonfile)
-        second_dict = {}
+        station_dict = {}
         counter = 1
-        for key in tqdm(data):
+        for key in tqdm(data, desc="Filtering Station Data: "):
             temp_dict = {
                 "id": key["id"],
                 "name": key["name"],
@@ -76,21 +79,46 @@ def run():
                 "is_planet": key["is_planetary"],
                 "station_type": key["type"],
             }
-            # for system in temp_dict_2:
-            #     if system['id'] == temp_dict['system_id']:
-            #         temp_dict.update(system)
             if (
                 temp_dict["max_landing"] == "L"
                 and temp_dict["has_repair"] is True
                 and temp_dict["is_planet"] is False
                 and temp_dict["station_type"] != "Fleet Carrier"
             ):
-                second_dict[counter] = temp_dict
+                station_dict[counter] = temp_dict
                 counter += 1
+        with open("EDDBFormatter/files/output/filtered_stations.json", "w") as json_file:
+            json.dump(station_dict, json_file, indent=2)
 
-        print("Read complete. Processing now...")
-        with open("EDDBFormatter/files/filtered_stations.json", "w") as json_file:
-            json.dump(second_dict, json_file, indent=2)
+    # I can use station_dict and system_dict now...
+    counter = 1
+    write_dict = {}
+    for key in tqdm(station_dict, desc="Combining System Files: "):
+        working_dict_1 = station_dict[counter]
+        try:
+            wd_2 = system_dict[station_dict[counter]['system_id']]
+        except KeyError:
+            counter += 1
+            continue
+        final_dict = {
+            "id": working_dict_1["id"],
+            "name": working_dict_1["name"],
+            "system_id": working_dict_1["system_id"],
+            "max_landing": working_dict_1["max_landing"],
+            "dist_star": working_dict_1["dist_star"],
+            "has_repair": working_dict_1["has_repair"],
+            "is_planet": working_dict_1["is_planet"],
+            "station_type": working_dict_1["station_type"],
+            "system_name": wd_2["system_name"],
+            "x_coord": wd_2["x_coord"],
+            "y_coord": wd_2["y_coord"],
+            "z_coord": wd_2["z_coord"],
+            "needs_permit": wd_2["needs_permit"],
+        }
+        write_dict[counter] = final_dict
+        counter += 1
+    with open("EDDBFormatter/files/output/filtered_combined_stations_with_systems.json", "w") as json_file:
+        json.dump(write_dict, json_file, indent=2)
 
 
 if __name__ == "__main__":
