a0e5445d5b90a95a4d139ca5c76dc2ec8ffee256
Joshua Salzedo <thhunkn0wnd@gmail.com>
2022-07-07 17:00:53 +0000

    Apply 2 suggestion(s) to 2 file(s)

--- halpybot/commands/ping.py
+++ halpybot/commands/ping.py
@@ -91,11 +91,11 @@ async def cmd_serverstat(ctx: Context, args: List[str]):
                 "https://hosting.zaonce.net/launcher-status/status.json"
             ) as response:
                 responses = await response.json()
-    except aiohttp.ClientError:
+    except aiohttp.ClientError as e:
         logger.exception("Error in Elite Server Status lookup.")
         raise EDSMConnectionError(
             "Unable to verify Elite Status, having issues connecting to the Elite API."
-        ) from aiohttp.ClientError
+        ) from e
     if len(responses) == 0:
         await ctx.reply("ERROR! Elite returned an empty reply.")
     else:
