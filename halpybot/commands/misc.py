7843eef8d5760c21c7ca3cd9602682b22f688d19
David Sangrey <rixxan@hullseals.space>
2022-07-30 14:55:08 -0400

    [Shorten] Add YOURLS Shortening Functionality

---
+++ halpybot/commands/misc.py
@@ -0,0 +1,40 @@
+"""
+misc.py - Miscellaneous Bot Functionality
+
+Copyright (c) The Hull Seals,
+All rights reserved.
+
+Licensed under the GNU General Public License
+See license.md
+"""
+from typing import List
+from loguru import logger
+from ..packages.utils import shorten
+from ..packages.checks import Require, Drilled
+from ..packages.command import Commands, get_help_text
+from ..packages.models import Context
+
+
+@Commands.command("shorten")
+@Require.permission(Drilled)
+async def cmd_shorten(ctx: Context, args: List[str]):
+    """
+    Shorten a given URL with the configured YOURLS API
+
+    Usage: !shorten --forceHTTP [url]
+    Aliases: n/a
+    """
+    force_http = False
+    if len(args) == 0:
+        return await ctx.reply(get_help_text("shorten"))
+    if args[0] == "--forceHTTP":
+        force_http = True
+        del args[0]
+        if not args:
+            return await ctx.reply(get_help_text("lookup"))
+    if force_http:
+        surl = await shorten(args[0], force_http=True)
+    else:
+        surl = await shorten(args[0])
+    return await ctx.reply(f"{ctx.sender}: Here's your short URL: {surl}.")
+
---
+++ halpybot/packages/utils/shorten.py
@@ -0,0 +1,76 @@
+"""
+shorten.py - YOURLS URL shortener
+
+Copyright (c) The Hull Seals,
+All rights reserved.
+
+Licensed under the GNU General Public License
+See license.md
+"""
+import aiohttp
+from loguru import logger
+from halpybot import DEFAULT_USER_AGENT
+from ..configmanager import config
+
+
+class YOURLSError(Exception):
+    """
+    Base class for YOURLS link errors
+    """
+
+
+class YOURLSNoResponse(YOURLSError):
+    """
+    An exception occurred while sending data to or receiving from a YOURLS API
+    """
+
+
+class YOURLSBadResponse(YOURLSError):
+    """
+    YOURLS returned an unprocessable response.
+    """
+
+
+async def shorten(url, force_http=False):
+    """
+    Shorten a given URL via a YOURLS passwordless API call
+
+    Args:
+        url (str): The URL to shorten
+        force_http (bool): Force YOURLS to reformat the url with http if not provided.
+
+    Returns:
+        surl (str): The shortened URL
+
+    Raises:
+        YOURLSNoResponse: YOURLS did not respond by the timeout.
+    """
+    if not url.lower().startswith("http") and not force_http:
+        url = "https://" + url
+    if not url.lower().startswith("http") and force_http:
+        url = "http://" + url
+
+    try:
+        async with aiohttp.ClientSession(
+            headers={"User-Agent": DEFAULT_USER_AGENT}
+        ) as session:
+            async with await session.get(
+                f"{config['YOURLS']['uri']}/yourls-api.php",
+                params={
+                    "signature": config["YOURLS"]["pwd"],
+                    "action": "shorturl",
+                    "format": "json",
+                    "url": url,
+                },
+                timeout=10,
+            ) as response:
+                responses = await response.json()
+    except aiohttp.ClientError as ex:
+        logger.exception("YOURLS Did Not Respond")
+        raise YOURLSNoResponse from ex
+
+    if not responses:
+        raise YOURLSNoResponse
+    if "shorturl" not in responses:
+        raise YOURLSBadResponse
+    return responses["shorturl"]
