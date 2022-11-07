73e83991a391a43b8def1175c5ecb2941e2a0474
David Sangrey <rixxan@hullseals.space>
2022-11-07 14:14:22 -0500

    [Minor] Add the Dice Roller
    Add a Dice Roller and remove old Help code that is GNDN.

--- halpybot/commands/misc.py
+++ halpybot/commands/misc.py
@@ -7,6 +7,8 @@ All rights reserved.
 Licensed under the GNU General Public License
 See license.md
 """
+import re
+import random
 from typing import List
 from loguru import logger
 from ..packages.utils import shorten
@@ -29,3 +31,27 @@ async def cmd_shorten(ctx: Context, args: List[str]):
     logger.info(f"{ctx.sender} requested shortening of {args[0]}")
     surl = await shorten(args[0])
     return await ctx.reply(f"{ctx.sender}: Here's your short URL: {surl}")
+
+
+@Commands.command("roll")
+async def cmd_roll(ctx: Context, args: List[str]):
+    """
+    Roll the dice!
+
+    Usage: !roll #d#
+    Aliases: n/a
+    """
+    if not args:
+        return await ctx.reply(get_help_text("roll"))
+    dicesearch = re.search(r"^\d+d\d+$", args[0])
+    if dicesearch is not None:
+        dice = args[0].split("d")
+        if int(dice[0]) > 10:
+            return await ctx.reply("Can't roll more than 10 dice at a time!")
+        rolls = []
+        for roll in range(int(dice[0])):
+            dice_roll = random.randint(1, int(dice[1]))
+            rolls.append(dice_roll)
+        total = sum(rolls)
+        return await ctx.reply(f"{ctx.sender}: {total} ({str(rolls)})")
+    return await ctx.reply(get_help_text("roll"))
