e88d7f7866308826561195a5dcaabb2474a8f202
David Sangrey <rixxan@hullseals.space>
2023-06-21 16:39:26 -0400

    [Halpy-340] Audit Run

--- halpybot/server/auth.py
+++ halpybot/server/auth.py
@@ -40,35 +40,34 @@ def authenticate(function):
     @functools.wraps(function)
     async def guarded(request):
         # Add Connection Logger
-        connection_logger = logger.bind(task="API")
+        with logger.contextualize(task="API"):
+            data = await request.json()
+            clientmac = request.headers.get("hmac")
+            key_check = request.headers.get("keyCheck")
 
-        data = await request.json()
-        clientmac = request.headers.get("hmac")
-        key_check = request.headers.get("keyCheck")
-
-        msg = json.dumps(data)
-        # Remove all whitespace for the purpose of ensuring identical inputs to HMAC
-        msg = "".join(msg.split())
-        mac = get_hmac(msg)
-        check = hmac.new(
-            bytes(config.api_connector.key.get_secret_value(), "utf8"),
-            msg=config.api_connector.key_check_constant.get_secret_value().encode(
-                "utf8"
-            ),
-            digestmod=hashlib.sha256,
-        )
-        # Check to see if the key is correct using static message.
-        # If wrong, return 401 unauthorised
-        if not hmac.compare_digest(key_check, check.hexdigest()):
-            connection_logger.warning(
-                "Failed authentication. Incorrect key or key verification message"
+            msg = json.dumps(data)
+            # Remove all whitespace for the purpose of ensuring identical inputs to HMAC
+            msg = "".join(msg.split())
+            mac = get_hmac(msg)
+            check = hmac.new(
+                bytes(config.api_connector.key.get_secret_value(), "utf8"),
+                msg=config.api_connector.key_check_constant.get_secret_value().encode(
+                    "utf8"
+                ),
+                digestmod=hashlib.sha256,
             )
-            raise web.HTTPUnauthorized()
-        # If the key is correct but HMAC is different, the body has been altered in transit and should be rejected
-        if not hmac.compare_digest(clientmac, mac.hexdigest()):
-            connection_logger.warning("Failed authentication. Bad request body")
-            raise web.HTTPUnprocessableEntity()
-        connection_logger.info("Successfully authenticated API request")
+            # Check to see if the key is correct using static message.
+            # If wrong, return 401 unauthorised
+            if not hmac.compare_digest(key_check, check.hexdigest()):
+                logger.warning(
+                    "Failed authentication. Incorrect key or key verification message"
+                )
+                raise web.HTTPUnauthorized()
+            # If the key is correct but HMAC is different, the body has been altered in transit and should be rejected
+            if not hmac.compare_digest(clientmac, mac.hexdigest()):
+                logger.warning("Failed authentication. Bad request body")
+                raise web.HTTPUnprocessableEntity()
+            logger.info("Successfully authenticated API request")
         return await function(request)
 
     return guarded
--- halpybot/server/server.py
+++ halpybot/server/server.py
@@ -30,52 +30,52 @@ class HalpyServer(web.Application):
         """A method to filter out spam requests that would otherwise result
         in a large error message and log them neatly"""
         # Add Connection Logger
-        connection_logger = logger.bind(task="API")
-        # If they don't provide authentication, we log it and return 400
-        request_method = request.method
-        request_path = request.path
+        with logger.contextualize(task="API"):
+            # If they don't provide authentication, we log it and return 400
+            request_method = request.method
+            request_path = request.path
 
-        if request_method == "POST":
-            if (
-                request.headers.get("hmac") is None
-                or request.headers.get("keyCheck") is None
-            ):
-                connection_logger.info("Request submitted with incomplete auth headers")
-                return HTTPBadRequest
+            if request_method == "POST":
+                if (
+                    request.headers.get("hmac") is None
+                    or request.headers.get("keyCheck") is None
+                ):
+                    logger.info("Request submitted with incomplete auth headers")
+                    return HTTPBadRequest
 
-        no_method = True
-        registered_routes = self.router.routes()
-        for route in registered_routes:
-            if route.method == request_method:
-                no_method = False
-                # The amount of time I spent looking through the documentation for where they keep the list of routes
-                # The route.resource.canonical SHOULD (tm) be the path for the route
-                if route.resource.canonical == request_path:
-                    return None
-        if no_method:
-            connection_logger.info("API request made for not used method")
-            return HTTPMethodNotAllowed(
-                request_method, list({route.method for route in routes})
-            )
-        connection_logger.info("API request made with unused path")
+            no_method = True
+            registered_routes = self.router.routes()
+            for route in registered_routes:
+                if route.method == request_method:
+                    no_method = False
+                    # The amount of time I spent looking through the documentation for where they keep the list of routes
+                    # The route.resource.canonical SHOULD (tm) be the path for the route
+                    if route.resource.canonical == request_path:
+                        return None
+            if no_method:
+                logger.info("API request made for not used method")
+                return HTTPMethodNotAllowed(
+                    request_method, list({route.method for route in routes})
+                )
+            logger.info("API request made with unused path")
         return HTTPNotFound()
 
     async def _handle(self, request: Request) -> StreamResponse:
         # Add Connection Logger
-        connection_logger = logger.bind(task="API")
-        try:
-            request_error = await self.__filter_request(request)
-            if request_error is not None:
-                connection_logger.info(
-                    "Invalid request submitted by {host} not processed",
-                    host=request.host,
-                )
-                raise request_error
-            response = await super()._handle(request)
-            response.headers.add("Server", "HalpyBOT")
-            return response
-        except web.HTTPError as ex:
-            return ex
+        with logger.contextualize(task="API"):
+            try:
+                request_error = await self.__filter_request(request)
+                if request_error is not None:
+                    logger.info(
+                        "Invalid request submitted by {host} not processed",
+                        host=request.host,
+                    )
+                    raise request_error
+                response = await super()._handle(request)
+                response.headers.add("Server", "HalpyBOT")
+                return response
+            except web.HTTPError as ex:
+                return ex
 
 
 @web.middleware
