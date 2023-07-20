3f6a73afeb9b24a1e3c3f0d8dfc7de0f4780a51d
David Sangrey <rixxan@hullseals.space>
2023-07-19 20:55:41 -0400

    [Halpy-347] Additional Validation

--- halpybot/packages/utils/spansh.py
+++ halpybot/packages/utils/spansh.py
@@ -7,7 +7,9 @@ All rights reserved.
 Licensed under the GNU General Public License
 See license.md
 """
+import string
 from typing import List
+from urllib.parse import quote
 import asyncio
 import aiohttp
 from loguru import logger
@@ -15,6 +17,17 @@ from halpybot import config
 from . import shorten, web_get
 from ..exceptions import SpanshNoResponse, SpanshBadResponse, SpanshResponseTimedOut
 from ..models import Context, Points
+
+
+def sanitize_system_name(name: str) -> str:
+    """
+    Makes an effort to sanitize system input to help prevent SSRF or other web vulnerabilities
+    """
+    # Whitelist allowed characters (alphanumeric, hyphen, space)
+    allowed_chars = f"- {string.ascii_letters}{string.digits}"
+    # Remove any disallowed characters from the system name
+    sanitized_name = "".join(c for c in name if c in allowed_chars)
+    return sanitized_name.strip()
 
 
 async def spansh_get_routes(
@@ -34,11 +47,10 @@ async def spansh_get_routes(
         None
 
     Raises:
+        aiohttp.ClientError: Error occurred during HTTP Request
         SpanshNoResponse: spansh did not respond in time.
         SpanshBadResponse: Spansh returned an unprocessable response
         SpanshResponseTimedOut: Spansh took too long to calculate a route
-
-        Unable to find route
     """
     spansh_loop_timeout = config.spansh.calculations_timeout
     job_results = []
@@ -51,34 +63,33 @@ async def spansh_get_routes(
                     "spansh did not respond while trying to receive the calculation results"
                 )
                 raise SpanshNoResponse from ex
-            if "error" in responses:
-                # Something went wrong, presumably on spansh's end since we checked all variables
-                # and there were no errors when creating the job
-                logger.warning(f"Spansh encountered an error: {responses['error']}")
-                return await ctx.reply(
-                    "Spansh encountered an error processing and was unable to continue."
-                )
-            if responses["status"] == "ok":
-                # Spansh has finised calculations for this job, add all inidvidual jump counts together
-                jumps = 0
-                for entry in responses["result"]["system_jumps"]:
-                    jumps += entry["jumps"]
-                job_results.append(jumps)
-                break  # Processing of this job has finished, start processing next job or respond with results
+            try:
+                if responses["status"] == "ok":
+                    # Spansh has finised calculations for this job, add all inidvidual jump counts together
+                    jumps = sum(
+                        entry["jumps"] for entry in responses["result"]["system_jumps"]
+                    )
+                    job_results.append(jumps)
+                    # Processing of this job has finished, start processing next job or respond with results
+                    break
+            except KeyError as keyerr:
+                logger.warning("Spansh returned an unprocessable response")
+                raise SpanshBadResponse from keyerr
             if spansh_loop_timeout <= 0:
                 logger.exception("spansh took too long to calculate a route")
                 raise SpanshResponseTimedOut
             # Wait 1 second to not send too many requests while waiting for spansh and update timeout counter
             spansh_loop_timeout -= 1
             await asyncio.sleep(1)
+    # Mention user since it may have been multiple seconds since they sent the calculation request
     await ctx.reply(
         f"{ctx.sender}: It will take about {job_results[0]} normal jumps or {job_results[1]} spansh jumps to get from "
         f"{points.point_a.pretty} to {points.point_b.pretty} with a range of {points.jump_range} LY."
     )  # Mention user since it may have been multiple seconds since they sent the calculation request
 
     # Encode spaces to be URL compatible
-    url_a = points.point_a.name.replace(" ", "%20")
-    url_b = points.point_b.name.replace(" ", "%20")
+    url_a = quote(sanitize_system_name(points.point_a.name))
+    url_b = quote(sanitize_system_name(points.point_b.name))
     # Format spansh results URL with parameters
     short = f"{config.spansh.page_endpoint}/{jobs[1]}?efficiency=60&from={url_a}&to={url_b}&range={points.jump_range}"
     if config.yourls.enabled:
@@ -108,8 +119,8 @@ async def spansh(ctx: Context, points: Points) -> None:
         params = {
             "efficiency": percent,
             "range": points.jump_range,
-            "from": points.point_a.name,
-            "to": points.point_b.name,
+            "from": sanitize_system_name(points.point_a.name),
+            "to": sanitize_system_name(points.point_b.name),
         }
         try:  # Try to start Jump Calculations
             responses = await web_get(config.spansh.route_endpoint, params)
